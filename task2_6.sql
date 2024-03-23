6cd8efe8be670fb98e0918419ae4a2a99013c93e
AlperenTasdelen <alperentasdelen51@gmail.com>
2024-03-23 19:07:18 +0300

    task2_6 not finished

--- task2_6.sql
+++ task2_6.sql
@@ -33,4 +33,51 @@
     *refunds*
     order_id
     reason
+
+     For each category find the product id that has been ordered most. Return the category,
+    product id, amount and the difference of this amount compared to the previous category in
+    decreasing order of amount. You must solve this using “lag” function.
 */
+
+WITH OrderedProducts AS (
+    SELECT
+        p.product_id,
+        p.category_id,
+        o.customer_id,
+        sc.amount AS amount,
+        ROW_NUMBER() OVER(PARTITION BY p.category_id, p.product_id ORDER BY sc.amount DESC) AS row_num
+    FROM
+        products p
+    INNER JOIN
+        shopping_carts sc ON p.product_id = sc.product_id
+    INNER JOIN
+        orders o ON sc.order_id = o.order_id
+    WHERE
+        o.status = 'COMPLETED'
+),
+
+ProductWithPreviousAmount AS (
+    SELECT
+        op.product_id,
+        op.category_id,
+        op.amount,
+        LAG(op.amount) OVER (PARTITION BY op.category_id ORDER BY op.amount DESC) AS previous_amount
+    FROM
+        OrderedProducts op
+    WHERE
+        op.row_num = 1
+)
+
+/*SELECT * FROM OrderedProducts;*/
+
+SELECT
+    pc.name AS category,
+    pwa.product_id,
+    pwa.amount,
+    pwa.amount - COALESCE(pwa.previous_amount, 0) AS amount_difference
+FROM
+    ProductWithPreviousAmount pwa
+INNER JOIN
+    product_categories pc ON pwa.category_id = pc.category_id
+ORDER BY
+    pwa.amount DESC;
