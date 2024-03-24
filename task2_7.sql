c44b35906a31834431e378be47b170f1681eaa0f
AlperenTasdelen <alperentasdelen51@gmail.com>
2024-03-24 22:02:01 +0300

    task2_7 not finished

--- task2_7.sql
+++ task2_7.sql
@@ -34,30 +34,42 @@
     order_id
     reason
 
-     Write a single SQL query that computes the statistics present in a cross tabulation over
+    Write a single SQL query that computes the statistics present in a cross tabulation over
     genders and months. The aggregate value reported should be total money spent for each
     gender-month pair. Each row should contain gender, month name, total money spent. You
     must solve this using “cube” function. Also write a crosstab function with this query.
 */
 
+WITH gender_month_spent AS (
+    SELECT 
+        COALESCE(c.gender, 'Total') AS gender,
+        TO_CHAR(o.order_time, 'Month') AS month_name,
+        SUM(p.price * sc.amount) AS total_money_spent
+    FROM 
+        orders o
+    JOIN 
+        shopping_carts sc ON o.order_id = sc.order_id
+    JOIN 
+        products p ON sc.product_id = p.product_id
+    JOIN 
+        customers c ON o.customer_id = c.customer_id
+    WHERE 
+        o.status = 'COMPLETED'
+    GROUP BY 
+        CUBE(c.gender, TO_CHAR(o.order_time, 'Month'))
+)
 SELECT 
-    COALESCE(c.gender, 'Total') AS gender,
-    COALESCE(EXTRACT(MONTH FROM o.order_time), 0) AS month,
-    SUM(p.price * sc.amount) AS total_money_spent
+    gender,
+    month_name,
+    total_money_spent AS total_money_spent,
 FROM 
-    orders o
-JOIN 
-    customers c ON o.customer_id = c.customer_id
-JOIN 
-    shopping_carts sc ON o.order_id = sc.order_id
-JOIN 
-    products p ON sc.product_id = p.product_id
-WHERE 
-    o.status = 'COMPLETED'
-GROUP BY 
-    CUBE(c.gender, EXTRACT(MONTH FROM o.order_time))
+    gender_month_spent
 ORDER BY 
-    c.gender, EXTRACT(MONTH FROM o.order_time);
+    gender NULLS LAST, 
+    month_name NULLS LAST;
+
+
+
 /*
 CREATE OR REPLACE FUNCTION gender_month_cross_tab() RETURNS TABLE (gender TEXT, month INT, total_money_spent NUMERIC) AS $$
 BEGIN
