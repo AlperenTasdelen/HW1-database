2c30af289e375c4504dfef1763ade6aeb1cee67a
AlperenTasdelen <alperentasdelen51@gmail.com>
2024-03-24 23:40:21 +0300

    task2_7 done

--- task2_7.sql
+++ task2_7.sql
@@ -42,7 +42,7 @@
 
 WITH gender_month_spent AS (
     SELECT 
-        COALESCE(c.gender, 'Total') AS gender,
+        COALESCE(c.gender, NULL) AS gender,
         TO_CHAR(o.order_time, 'Month') AS month_name,
         SUM(p.price * sc.amount) AS total_money_spent
     FROM 
@@ -53,47 +53,15 @@ WITH gender_month_spent AS (
         products p ON sc.product_id = p.product_id
     JOIN 
         customers c ON o.customer_id = c.customer_id
-    WHERE 
-        o.status = 'COMPLETED'
     GROUP BY 
         CUBE(c.gender, TO_CHAR(o.order_time, 'Month'))
 )
 SELECT 
     gender,
-    month_name,
-    total_money_spent AS total_money_spent,
+    month_name AS month,
+    total_money_spent AS cart_total
 FROM 
     gender_month_spent
 ORDER BY 
     gender NULLS LAST, 
     month_name NULLS LAST;
-
-
-
-/*
-CREATE OR REPLACE FUNCTION gender_month_cross_tab() RETURNS TABLE (gender TEXT, month INT, total_money_spent NUMERIC) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT 
-        COALESCE(c.gender, 'Total') AS gender,
-        COALESCE(EXTRACT(MONTH FROM o.order_time), 0) AS month,
-        SUM(p.price * sc.amount) AS total_money_spent
-    FROM 
-        orders o
-    JOIN 
-        customers c ON o.customer_id = c.customer_id
-    JOIN 
-        shopping_carts sc ON o.order_id = sc.order_id
-    JOIN 
-        products p ON sc.product_id = p.product_id
-    WHERE 
-        o.status = 'COMPLETED'
-    GROUP BY 
-        CUBE(c.gender, EXTRACT(MONTH FROM o.order_time))
-    ORDER BY 
-        c.gender, EXTRACT(MONTH FROM o.order_time);
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT * FROM gender_month_cross_tab();
-*/
